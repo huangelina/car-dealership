f5a896f379a2d2ec1d5d000225da751be3e11476
Jay Parfenchuck <j.chuck@live.com>
2023-10-23 18:41:47 -0400

    admin.py set up

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Salesperson, AutomobileVO, Sale
 # Register your models here.
+
+
+admin.site.register(Salesperson)
+admin.site.register(AutomobileVO)
+admin.site.register(Sale)
