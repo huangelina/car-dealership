8a3b6e58460a4abf255538b46779e6c0674bbb75
Jay Parfenchuck <j.chuck@live.com>
2023-10-23 18:24:28 -0400

    poller complete

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -11,19 +11,27 @@ django.setup()
 
 # Import models from sales_rest, here.
 # from sales_rest.models import Something
+from sales_rest.models import AutomobileVO
 
 
 def poll(repeat=True):
     while True:
         print('Sales poller polling for data')
         try:
-            # Write your polling logic, here
-            # Do not copy entire file
-
-            pass
+            response = requests.get('http://project-beta-inventory-api-1:8000/api/automobiles/')
+            content = json.loads(response.content)
+            for automobile in content["autos"]:
+                AutomobileVO.objects.update_or_create(
+                    import_href=automobile["href"],
+                    defaults={
+                        "vin": automobile["vin"],
+                        "sold": automobile["sold"],
+                    }
+                )
         except Exception as e:
+            print("exception happened!")
             print(e, file=sys.stderr)
-        
+
         if (not repeat):
             break
 
