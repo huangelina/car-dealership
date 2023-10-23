461a34202307a396b796b0dabf1c7d67a3e22044
Jay Parfenchuck <j.chuck@live.com>
2023-10-23 18:07:41 -0400

    made models , incorporated a decimalfield for cents

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200, null=True)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, null=True)
+    vin = models.CharField(max_length=100)
+    sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.vin
+
+    def sell(self):
+        self.sold = True
+        self.save()
+
+
+class Sale(models.Model):
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sales",
+        on_delete=models.CASCADE,
+    )
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="salesperson",
+        on_delete=models.CASCADE,
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="customer",
+        on_delete=models.CASCADE,
+    )
+    price = models.DecimalField(decimal_places=2, max_digits=20, null=True)
