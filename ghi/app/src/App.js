9bb0a53edd762ae249448728bf07b4a55f41670d
Jay Parfenchuck <j.chuck@live.com>
2023-10-23 21:48:51 -0400

    added sales front end pages, add nav, and app for them

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import SalesPeopleList from './SalesPeopleList';
+import SalesPersonForm from './SalesPersonForm';
+import SalesPersonHistory from './SalesPersonHistory';
+import CustomerForm from './CustomerForm';
+import CustomerList from './CustomerList';
+import SalesList from './SalesList';
+import SalesForm from './SalesForm';
+
 
 function App() {
   return (
@@ -9,6 +17,19 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="salespeople">
+            <Route index element={<SalesPeopleList />} />
+            <Route path="create" element={<SalesPersonForm />} />
+            <Route path="history" element={<SalesPersonHistory />} />
+          </Route>
+          <Route path="customers">
+            <Route index element={<CustomerList />} />
+            <Route path="create" element={<CustomerForm />} />
+          </Route>
+          <Route path="sales">
+            <Route index element={<SalesList />} />
+            <Route path="create" element={<SalesForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
